49be620b6779b221126f328870694095ac9677e2
danaszumigraj <dana.szumigraj@gmail.com>
2022-02-01 16:45:46 -0300

    Ejercicios completos

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -3,15 +3,59 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var resultado;
+
 function Sumar () 
 {
-	
+
+	precioUno = document.getElementById("txtIdPrecioUno").value;
+	precioDos = document.getElementById("txtIdPrecioDos").value;
+	precioTres = document.getElementById("txtIdPrecioTres").value;
+
+	precioUno = parseFloat(precioUno);
+	precioDos = parseFloat(precioDos);
+	precioTres = parseFloat(precioTres);
+
+	resultado = precioUno + precioDos + precioTres
+
+	alert("La suma es " + resultado)
 }
 function Promedio () 
 {
+
+	precioUno = document.getElementById("txtIdPrecioUno").value;
+	precioDos = document.getElementById("txtIdPrecioDos").value;
+	precioTres = document.getElementById("txtIdPrecioTres").value;
+
+	precioUno = parseFloat(precioUno);
+	precioDos = parseFloat(precioDos);
+	precioTres = parseFloat(precioTres);
+
+	resultado = (precioUno + precioDos + precioTres) / 3
+
+	alert("El promedio es " + resultado)
 	
 }
 function PrecioFinal () 
 {
-	
+	var impuesto;
+
+	precioUno = document.getElementById("txtIdPrecioUno").value;
+	precioDos = document.getElementById("txtIdPrecioDos").value;
+	precioTres = document.getElementById("txtIdPrecioTres").value;
+
+	precioUno = parseFloat(precioUno);
+	precioDos = parseFloat(precioDos);
+	precioTres = parseFloat(precioTres);
+
+	resultado = precioUno + precioDos + precioTres
+	impuesto = 21 / 100
+
+	resultado = resultado + (resultado * impuesto)
+
+	alert("El precio final es " + resultado)
+
 }
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,13 +6,71 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var largoTerreno;
+	var anchoTerreno;
+	var hiloAlambre;
+	var resultado;
+
+	largoTerreno = document.getElementById("txtIdLargo").value;
+	anchoTerreno = document.getElementById("txtIdAncho").value;
+
+	largoTerreno = parseFloat(largoTerreno);
+	anchoTerreno = parseFloat(anchoTerreno);
+
+	hiloAlambre = 3;
+	largoTerreno = largoTerreno * 2;
+	anchoTerreno = anchoTerreno * 2;
+
+	resultado = (largoTerreno + anchoTerreno) * hiloAlambre;
+
+	alert("La cantidad de hilos a comprar es " + resultado);
+
+
 
 }
 function Circulo () 
 {
+	var radioTerreno;
+	var circunferencia
+	var hiloAlambre;
+	var resultado;
+
+	radioTerreno = document.getElementById("txtIdRadio").value;
+
+	radioTerreno = parseFloat(radioTerreno);
+
+	hiloAlambre = 3;
 	
+	circunferencia = radioTerreno * 2 * 3,14;
+	resultado = circunferencia * hiloAlambre
+
+	alert("La cantidad de hilos a comprar es " + resultado);
 }
 function Materiales () 
 {
+	var largoTerreno
+	var anchoTerreno
+	var areaTerreno
+	var bolsaCemento
+	var bolsaCal
+
+	largoTerreno = document.getElementById("txtIdLargo").value;
+	anchoTerreno = document.getElementById("txtIdAncho").value;
+
+	largoTerreno = parseFloat(largoTerreno);
+	anchoTerreno = parseFloat(anchoTerreno);
+
+	areaTerreno = largoTerreno*anchoTerreno;
+
+	bolsaCemento = 2*areaTerreno;
+	bolsaCal = 3*areaTerreno;
+
+	bolsaCemento = parseInt(bolsaCemento);
+	bolsaCal = parseInt(bolsaCal);
+
+	alert("Será necesario comprar " + bolsaCemento + (" bolsa(s) de cemento y ") + bolsaCal + (" bolsa(s) de cal."))
+
+
+
 	
 }
