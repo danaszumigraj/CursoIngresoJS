a4e7182d6852b692212e62b23fa06eba465b1c24
danaszumigraj <dana.szumigraj@gmail.com>
2022-02-11 12:36:23 -0300

    tp 4 completo sin switch

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -12,167 +12,74 @@ function CalcularPrecio ()
 {
  	var cantidadLamparas = parseInt(document.getElementById("txtIdCantidad").value);
     var marcaLamparas = document.getElementById("Marca").value;
-    var precioLampara = 35;
+    var precioLampara;
+    var descuento 
+    var precioFinal 
+    var impuesto 
 
-    switch (marcaLamparas)
-     {
-         case "ArgentinaLuz":
-            if(cantidadLamparas = 5)
-                {
-                    alert("El descuento es del 40%, precio final: " + (precioLampara * cantidadLamparas)*40/100);
-                }
+    precioLampara = 35 
+    precioFinal = precioLampara * cantidadLamparas
 
-            else if(cantidadLamparas = 4)
-                {
-                    alert("El descuento es del 25%, precio final: " + (precioLampara * cantidadLamparas)*25/100);
-                }
-                else if (cantidadLamparas = 3)
-                    {
-                        alert("El descuento es del 15%, precio final: " + (precioLampara * cantidadLamparas)*15/100);
-                    }
-                    else
-                    {
-                        alert("No hay descuento, precio final: " + precioLampara * cantidadLamparas)
-                    }
-        break;
 
-        case "FelipeLamparas":
-            if(cantidadLamparas = 5)
-                {
-                    alert("El descuento es del 30%, precio final: " + (precioLampara * cantidadLamparas)*30/100);
-                }
-            else if(cantidadLamparas = 4)
-            {
-                alert("El descuento es del 25%, precio final: " + (precioLampara * cantidadLamparas)*25/100);
-            }
-                else if (cantidadLamparas = 3)
-                {
-                    alert("El descuento es del 10%, precio final: " + (precioLampara * cantidadLamparas)*10/100)
-                }
-                else
-                    {
-                        alert("No hay descuento, precio final: " + precioLampara * cantidadLamparas)
-                    }
-        break;
-        
-        case "JeLuz":
-        case "HazIluminacion":
-        case "Osram":
-            if(cantidadLamparas = 5)
-                {
-                    alert("El descuento es del 30%, precio final: " + (precioLampara * cantidadLamparas)*30/100);
-                }
-            else if(cantidadLamparas = 4)
-                {
-                    alert("El descuento es del 20%, precio final: " + (precioLampara * cantidadLamparas)*20/100);
-                }
-                else if (cantidadLamparas = 3)
-                {
-                    alert("El descuento es del 5%, precio final: " + (precioLampara * cantidadLamparas)*5/100)
-                }
-                else
-                    {
-                        alert("No hay descuento, precio final: " + precioLampara * cantidadLamparas)
-                    }
-        break;
 
-        default:
-            alert("El descuento es del 50%, precio final: " + (precioLampara * cantidadLamparas)*50/100)
-        
-     }
 
-     /*{
-
-    //Declaro variables
-    var precioLamparas;
-    var marca;
-    var descuento;
-    var precioFinal;
-    var cantLamparas;
-    var impuesto;
-    var precio;
-    
-    //ID
-    marca = document.getElementById("Marca").value;
-    cantLamparas = document.getElementById("txtIdCantidad").value;
-    
-
-    
-    //Parseo
-    cantLamparas = parseInt(cantLamparas);
-    precio = parseFloat(precio);
-    
-    
-
-    //Proceso 
-    precio = 35;
-    precioFinal = precio * cantLamparas; 
-    descuento = 0;
-    
-    //IF
-
-    //A
-    if (cantLamparas >5)
+    if (cantidadLamparas >= 6)
     {
         descuento = (precioFinal * 50/100);
     }
-    else if (cantLamparas == 5)
+    else if (cantidadLamparas == 5)
     {
-        if(marca == "ArgentinaLuz")
+        if(marcaLamparas == "ArgentinaLuz")
         {
-            descuento = (precioFinal * 40/100);
+
+            descuento = precioFinal * 40/100;
         }
         else
         {
-            descuento = (precioFinal * 30/100);
+            descuento = precioFinal * 30/100;
         }
     }
-
-    //C
-  
-     else if (cantLamparas == 4)
+    else  if (cantidadLamparas == 4)
     {
-        if (marca == "ArgentinaLuz" || marca == "FelipeLamparas" )
+        if(marcaLamparas =='ArgentinaLuz' || marcaLamparas == 'FelipeLamparas')
         {
-            descuento = (precioFinal * 25/100);
+            descuento = precioFinal * 25/100;
         }
         else
         {
-            descuento = (precioFinal * 20/100);
+            descuento = precioFinal * 20/100;
         }
     }
-    
-     //D
-
-     else if (cantLamparas == 3)
+    else if(cantidadLamparas == 3)
     {
-        if (marca == "ArgentinaLuz")
+       
+        if(marcaLamparas == 'ArgentinaLuz')
         {
-            descuento = (precioFinal * 15/100);
+       
+            descuento = precioFinal * 15/100;
         }
-        else if (marca == "FelipeLamparas")
-
-            descuento = (precioFinal * 10/100);
-        
+        else if(marcaLamparas =='FelipeLamparas')
+        {
+            descuento = precioFinal * 10/100;
+        }
         else
         {
-            descuento = (precioFinal * 5/100);
+            descuento = precioFinal * 5/100;
         }
     }
- 
-    //E
+    else 
+    {
+        descuento = 0
+    }
+
+    precioFinal = precioFinal - descuento;
+    document.getElementById("txtIdprecioDescuento").value = precioFinal;
+
     if ( precioFinal > 120 )
 	{
 		impuesto = precioFinal * 10/100;
 		precioFinal = precioFinal + impuesto;
-		alert("IIBB Usted pagó $" + precioFinal + " siendo $" + impuesto + " el impuesto que se pagó");
+		alert("Usted pago $" + impuesto + " de IIBB.");
 	}
-
-precioFinal = precioFinal - descuento;
-document.getElementById("txtIdprecioDescuento").value = precioFinal;
-}
-*/
-
      
-
 }
